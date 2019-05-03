a4fd26d7fa768696ac83371646c3638168a8e1e1
AlexandreCavanna <alexandre.cavanna.pro@gmail.com>
2019-05-03 21:46:46 +0200

    Update

--- js/main.js
+++ js/main.js
@@ -2,15 +2,13 @@ const myApi = Object.create(JCDecauxApi);
 $('.leaflet-tooltip').css('font-size', 30);
 let url = 'https://api.jcdecaux.com/vls/v1/stations?contract=';
 url = myApi.createUrl(url);
-console.log(url);
+//console.log(url);
 
 carte.initMap();
 ajax(url, function(resp) {
-  let stations = JSON.parse(resp);
-  console.log(resp);
-  stations.forEach(function(station){
-  carte.createMarker(station);
-  })
+    let stations = JSON.parse(resp);
+    //console.log(resp);
+    carte.createMarker(stations);
 });
 
  
--- js/map.js
+++ js/map.js
@@ -1,32 +1,32 @@
-
 carte = {
-    
-    viewMap : L.map('mapid').setView([45.764043, 4.835659], 13),
-    
-    initMap : function() {
+
+    viewMap: L.map('mapid').setView([45.764043, 4.835659], 13),
+
+    initMap: function() {
         L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
             attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
             maxZoom: 18,
             id: 'mapbox.streets',
             accessToken: 'pk.eyJ1IjoiaXNwaXJrIiwiYSI6ImNqdGc2M2ZxNzAwY3I0M3A5anlsZHBzNHYifQ.ZBDPL2DEHO5AKzMXUB1klQ',
-                   
+
         }).addTo(carte.viewMap);
     },
-    
-    createMarker : function(station) {
-       let marker = L.marker([station.position.lat, station.position.lng]).addTo(carte.viewMap);
-       marker.bindTooltip(station.name,{opacity : 1});
-       marker.on('click',function(e) {
-       carte.viewMap.setView(e.latlng,17);
-       });
+
+    createMarker: function(stations) {
+        markers = L.markerClusterGroup();
+        for (var i = 0; i < stations.length; i++) {
+            var adresse = stations[i].name;
+            var marker = L.marker(new L.LatLng(stations[i].position.lat, stations[i].position.lng)).on('click', function(e) {
+            carte.viewMap.setView(e.latlng, 17);
+                
+                
+                    document.querySelector("div#zone_reservation h3").textContent = station.name;
+                
+               
+            });
+            marker.bindPopup(adresse);
+            markers.addLayer(marker);
+        }
+        carte.viewMap.addLayer(markers);
     }
 }
-
-
-
-
-
-
-
-
-
