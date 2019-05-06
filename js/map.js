43162e9ac08737f07725075fbf0c954e6d3f895b
AlexandreCavanna <alexandre.cavanna.pro@gmail.com>
2019-05-06 18:00:25 +0200

    Ajout des informations Markers dans le DOM

--- js/map.js
+++ js/map.js
@@ -14,19 +14,24 @@ carte = {
 
     createMarker: function(stations) {
         markers = L.markerClusterGroup();
-        for (var i = 0; i < stations.length; i++) {
-            var adresse = stations[i].name;
-            var marker = L.marker(new L.LatLng(stations[i].position.lat, stations[i].position.lng)).on('click', function(e) {
-            carte.viewMap.setView(e.latlng, 17);
-                
-                
-                    document.querySelector("div#zone_reservation h3").textContent = station.name;
-                
-               
-            });
-            marker.bindPopup(adresse);
+        stations.forEach(station => {
+            var marker = L.marker(new L.LatLng(station.position.lat, station.position.lng));
+            marker.name = station.name;
+            marker.bindPopup(marker.name);
             markers.addLayer(marker);
-        }
+            marker.addEventListener("click", function(e) {
+            carte.viewMap.setView(e.latlng, 17);
+                                document.querySelector("div#zone_reservation h3").textContent = station.name;
+            var stationStatus = document.querySelector("#zone_reservation > p:nth-child(3)");
+                                document.querySelector("#zone_reservation > p:nth-child(5)").textContent = station.address;
+                                document.querySelector("#zone_reservation > p:nth-child(7)").textContent = station.available_bike_stands;
+                if ( station.status === 'OPEN') {
+                    stationStatus.textContent = "OUVERT";
+                } else {
+                    stationStatus.textContent = "FERMER";
+                }
+            })
+        });      
         carte.viewMap.addLayer(markers);
     }
 }
