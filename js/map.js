b8833f00f64f51da321223e49ec0158f94f47a43
AlexandreCavanna <alexandre.cavanna.pro@gmail.com>
2019-05-07 00:53:01 +0200

    Add unavailable markers and click event info DOM for each Station

--- js/map.js
+++ js/map.js
@@ -13,24 +13,58 @@ carte = {
     },
 
     createMarker: function(stations) {
+        var redIcon = L.icon({
+            iconUrl: 'images/red_marker.png',
+            shadowUrl: 'https://unpkg.com/leaflet@1.4.0/dist/images/marker-shadow.png',
+            iconSize:     [35, 40], // size of the icon
+             // size of the shadow
+            shadowAnchor: [11, 20],
+            popupAnchor:  [0, -15],
+        });
         markers = L.markerClusterGroup();
         stations.forEach(station => {
+           
             var marker = L.marker(new L.LatLng(station.position.lat, station.position.lng));
+            
             marker.name = station.name;
             marker.bindPopup(marker.name);
             markers.addLayer(marker);
+            if(station.available_bikes <= 0 || station.status === 'FERMER') {
+                marker.setIcon(redIcon);
+            };
             marker.addEventListener("click", function(e) {
             carte.viewMap.setView(e.latlng, 17);
                                 document.querySelector("div#zone_reservation h3").textContent = station.name;
             var stationStatus = document.querySelector("#zone_reservation > p:nth-child(3)");
-                                document.querySelector("#zone_reservation > p:nth-child(5)").textContent = station.address;
-                                document.querySelector("#zone_reservation > p:nth-child(7)").textContent = station.available_bike_stands;
+            var stationAddress = document.querySelector("#zone_reservation > p:nth-child(5)");
+            var stationBike = document.querySelector("#zone_reservation > p:nth-child(7)");
+            if (station.address === "") {
+                stationAddress.classList.add("text-primary");
+                stationAddress.textContent = "Pas d'adresse renseigné"
+               } else {
+                stationAddress.classList.add("text-primary");
+                stationAddress.textContent = station.address.toUpperCase();
+             
+               }
                 if ( station.status === 'OPEN') {
                     stationStatus.textContent = "OUVERT";
+                    stationStatus.classList.remove("text-danger");
+                    stationStatus.classList.add("text-success","font-weight-bold");
                 } else {
                     stationStatus.textContent = "FERMER";
+                    stationStatus.classList.add("text-danger", "font-weight-bold");
                 }
-            })
+               if (station.available_bikes > 0)  {
+                stationBike.textContent = station.available_bikes + " vélib(s) disponibles."
+                stationBike.classList.remove("text-danger");
+                stationBike.classList.add("text-success", "font-weight-bold");
+               } else {
+                stationBike.textContent = station.available_bikes + " vélib disponible.";
+                stationBike.classList.remove("text-success");
+                stationBike.classList.add("text-danger", "font-weight-bold");
+               } 
+              
+           })
         });      
         carte.viewMap.addLayer(markers);
     }
