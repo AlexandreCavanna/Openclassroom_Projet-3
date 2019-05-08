87eacea10e13fab740f3a15882a52a1fae876b37
AlexandreCavanna <alexandre.cavanna.pro@gmail.com>
2019-05-08 23:15:15 +0200

    update

--- js/map.js
+++ js/map.js
@@ -35,30 +35,11 @@ carte = {
             marker.addEventListener("click", function(e) {
                 carte.viewMap.setView(e.latlng, 17);
                 document.querySelector("div.card-header h3").textContent = station.name;
-<<<<<<< HEAD
-                var stationStatus = document.querySelector("div:nth-child(1) > p");
-=======
                 var stationStatus = document.querySelector("div.card-body div:nth-child(1)  p");
->>>>>>> master
                 var stationAddress = document.querySelector("div:nth-child(3) > p");
                 var stationBike = document.querySelector("div:nth-child(5) > p");
                 document.querySelector("div.card.text-center.my-3.ml-3.mr-3.text-center").style.display = "block";
                 document.querySelector("#alert-form").style.display = "none";
-<<<<<<< HEAD
-                var imgl = document.getElementsByClassName("leaflet-marker-icon");
-                if (imgl.getAttribute(src) == "https://unpkg.com/leaflet@1.4.0/dist/images/marker-icon.png") {
-                    var iconeMarkForm = document.querySelector("div:nth-child(3) > i");
-                    iconeMarkForm.remove("text-danger");
-                    iconeMarkForm.classList.add("text-primary");
-                }
-                if (station.address === "") {
-                    stationAddress.classList.add("text-primary");
-                    stationAddress.textContent = "Pas d'adresse renseigné"
-                } else {
-                    stationAddress.classList.add("text-primary");
-                    stationAddress.textContent = station.address.toUpperCase();
-
-=======
                 var icoClock = document.querySelector("div.card-body div:nth-child(1) i");
                 var icoMarker = document.querySelector("div.card-body div:nth-child(3) i");
                 var icoBike = document.querySelector("div.card-body div:nth-child(5) i");
@@ -74,17 +55,13 @@ carte = {
                     stationAddress.textContent = station.address.toUpperCase();
                     icoMarker.classList.remove("text-danger");
                     icoMarker.classList.add("text-primary");
->>>>>>> master
                 }
                 if (station.status === 'OPEN') {
                     stationStatus.textContent = "OUVERT";
                     stationStatus.classList.remove("text-danger");
                     stationStatus.classList.add("text-success", "font-weight-bold");
-<<<<<<< HEAD
-=======
                     icoClock.classList.remove("text-danger");
                     icoClock.classList.add("text-primary");
->>>>>>> master
                 } else {
                     stationStatus.textContent = "FERMER";
                     stationStatus.classList.add("text-danger", "font-weight-bold");
@@ -95,12 +72,6 @@ carte = {
                     stationBike.textContent = station.available_bikes + " vélib(s) disponibles."
                     stationBike.classList.remove("text-danger");
                     stationBike.classList.add("text-success", "font-weight-bold");
-<<<<<<< HEAD
-                } else {
-                    stationBike.textContent = station.available_bikes + " vélib disponible.";
-                    stationBike.classList.remove("text-success");
-                    stationBike.classList.add("text-danger", "font-weight-bold");
-=======
                     icoBike.classList.remove("text-danger");
                     icoBike.classList.add("text-primary");
                 } else {
@@ -109,20 +80,13 @@ carte = {
                     icoBike.classList.remove("text-primary");
                     icoBike.classList.add("text-danger");
                     stationBike.classList.add("text-danger", "font-weight-bold");      
->>>>>>> master
                 }
 
             })
             var buttonResetMap = document.getElementById("reset-map");
-<<<<<<< HEAD
-            buttonResetMap.addEventListener("click", function(e) {
-
-                carte.viewMap.setView([45.764043, 4.835659], 13);
-=======
             buttonResetMap.addEventListener("click", function() {
             carte.viewMap.setView([45.764043, 4.835659], 13);
             marker.closePopup();
->>>>>>> master
             })
         });
         carte.viewMap.addLayer(markers);
