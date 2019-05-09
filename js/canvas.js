cf5a44405e014e0657435ee45d8a4fe496bfe223
AlexandreCavanna <alexandre.cavanna.pro@gmail.com>
2019-05-09 18:46:08 +0200

    comments

--- js/canvas.js
+++ js/canvas.js
@@ -1,12 +1,12 @@
   createCanvas = {  
     mouseX: 0, // Variables pour la position de la souris
     mouseY: 0,
-    lastX: -1, // Variables pour la prÃ©cÃ©dente position de la souris
+    lastX: -1, // Variables pour la precedente position de la souris
     lastY: -1,
-    mouseDown: false, // Variable pour vÃ©rifer que le boutton de la souris soit appuyÃ©e pour commencer Ã  dessiner
+    mouseDown: false, // Variable pour verifer que le boutton de la souris soit appuyee pour commencer a  dessiner
     context: null,
 
-    initCanvas() { // Pour crÃ©er le canvas
+    initCanvas() { // Pour creer le canvas
         const canvas  = document.getElementById('canvas');
         this.context = canvas.getContext('2d');
         canvas.width = 310; // Largeur du canvas
@@ -14,7 +14,7 @@
         this.context.fillStyle = "#fff"; // Couleur de fond
         this.context.lineWidth = 2; // Epaisseur du trait
         this.context.strokeStyle = "black"; // Couleur du trait
-        this.context.lineCap = 'round'; // ExtrÃ©mitÃ© du trait
+        this.context.lineCap = 'round'; // Extremite du trait
         this.draw(); // Pour dessiner
         this.erase(); // Pour effacer le contenu du canvas
     },
@@ -27,8 +27,8 @@
             this.mouseY = e.offsetY; // Position sur l'axe Y
         }
         else if (e.layerX) {
-            this.mouseX = e.layerX; // Retourne les coordonnÃ©es sur l'axe verticale sur l'event en cours
-            this.mouseY = e.layerY; // Retourne les coordonnÃ©es sur l'axe horizontale sur l'event en cours
+            this.mouseX = e.layerX; // Retourne les coordonnees sur l'axe verticale sur l'event en cours
+            this.mouseY = e.layerY; // Retourne les coordonnees sur l'axe horizontale sur l'event en cours
         }
     },
 
@@ -49,9 +49,9 @@
                 this.lastX = x;                         // Le premier point et le point du clic
                 this.lastY = y;
             }
-        this.context.beginPath();                       // DÃ©but du chemin
-        this.context.moveTo(this.lastX, this.lastY);    // Point de dÃ©part
-        this.context.lineTo(x, y);                      // TracÃ© de la ligne
+        this.context.beginPath();                       // Debut du chemin
+        this.context.moveTo(this.lastX, this.lastY);    // Point de depart
+        this.context.lineTo(x, y);                      // Trace de la ligne
         this.context.closePath();                       // Fermeture du chemin
         this.context.stroke();                          
         this.lastX = x;                                
@@ -63,7 +63,7 @@
         canvas[0].addEventListener('mousedown', (e) => { 
             createCanvas.mouseDown = true;                                      // Quand la bouton de la souris est down
             createCanvas.getMousePos(e);                                        // On regarde sa position
-            createCanvas.drawLine(createCanvas.mouseX, createCanvas.mouseY);    // On commence Ã  dessiner
+            createCanvas.drawLine(createCanvas.mouseX, createCanvas.mouseY);    // On commence a  dessiner
         }, false);
 
         canvas[0].addEventListener('mousemove', (e) => {
@@ -75,11 +75,11 @@
 
         window.addEventListener('mouseup', () => {                          // Quand le bouton de la souris n'est plus down
             createCanvas.mouseDown = false;                                       // La souris n'est plus down
-            createCanvas.lastX = -1;                                              // La derniÃ¨re position de la souris est -1 pour indiquer qu'il y a un nouveau chemin
+            createCanvas.lastX = -1;                                              // La derniere position de la souris est -1 pour indiquer qu'il y a un nouveau chemin
             createCanvas.lastY = -1;                                                 
         }, false);
         
-        // MÃªme chose mais pour les tablettes et smartphones
+        // Meme chose mais pour les tablettes et smartphones
         canvas[0].addEventListener('touchstart', (e) => {
             createCanvas.getTouchPos();
             createCanvas.drawLine(createCanvas.touchX, createCanvas.touchY);
