15419c5d1801984257964dae47ffb50c9eff0218
AlexandreCavanna <alexandre.cavanna.pro@gmail.com>
2019-05-17 00:53:28 +0200

    final version of canvas on mobile

--- js/form.js
+++ js/form.js
@@ -1,20 +1,32 @@
 form = {
+    buttonReservation: document.getElementById("button-reservation"), // Bouton pour faire apparaÃ®tre le formulaire de rÃ©servation
+    buttonReservationSuccess: document.getElementById("reservation-success"), // Bouton pour valider la rÃ©servation
+    buttonReservationDelete: document.getElementById("delete-reservation"), // Bouton pour annuler la rÃ©servation en cours
+    modalReservation: document.getElementById("modal-reservation"), // Bouton pour afficher le modal de rÃ©servation
+    mapLegend: document.getElementById("map-legend"), // LÃ©gende de la carte
+    sectionReservation: document.getElementById("section-reservation"), // Section de la rÃ©servation
+    canvasClean: document.getElementById("canvas-delete"), // Bouton pour nettoyer le canvas
+    alertForm: document.getElementById("alert-form"), // Message d'alerte suivant la situation
+    sectionForm: document.getElementById("section-form"), // La section entiÃ¨re d'une rÃ©servation
+    reservationTitle: document.getElementById("reservation-title"), // Titre de la rÃ©servation en cours
+    reservationTime: document.getElementById("reservation-timer"), // Temps de la rÃ©servation en cours
+    reservationContainer: document.getElementById("reservation-container"), // Container de la rÃ©servation en cours
     manageForm: function() {
-        var buttonResetMap = document.getElementById("reset-map");
-        buttonResetMap.addEventListener("click", function() {
+        this.buttonResetMap = document.getElementById("reset-map");
+        this.buttonResetMap.addEventListener("click", function() {
             carte.viewMap.setView([45.764043, 4.835659], 13);
         })
-        buttonResetMap.onclick = function() {
+        this.buttonResetMap.onclick = function() {
             carte.viewMap.closePopup();
         }
 
-        var buttonReserve = document.getElementById("button-reservation");
-        var buttonErase = document.getElementById("canvas-delete");
-        var sectionReserve = document.getElementById("section-reservation");
-        var mapLegend = document.getElementById("map-legend");
-        var canvas = document.getElementById("canvas");
+        this.buttonReserve = document.getElementById("button-reservation");
+        this.buttonErase = document.getElementById("canvas-delete");
+        this.sectionReserve = document.getElementById("section-reservation");
+        this.mapLegend = document.getElementById("map-legend");
+        createCanvas.canvas = document.getElementById("canvas");
         
-        buttonReserve.addEventListener("click", function() {
+        this.buttonReserve.addEventListener("click", function() {
             buttonReserve.style.display = "none";
             mapLegend.style.display = "none";
             sectionReserve.style.display = "block";
