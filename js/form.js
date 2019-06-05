4497a5acec807992c8130c57f6a9d0ac509582d4
AlexandreCavanna <alexandre.cavanna.pro@gmail.com>
2019-06-05 14:56:51 +0200

    Add booking

--- js/form.js
+++ js/form.js
@@ -1,27 +1,160 @@
 form = {
-    manageForm: function() {
-        var buttonResetMap = document.getElementById("reset-map");
-        buttonResetMap.addEventListener("click", function() {
+    buttonReserve: document.getElementById("button-reservation"),
+    buttonErase: document.getElementById("canvas-delete"),
+    buttonReservationDelete: document.getElementById("delete-reservation"),
+    buttonSucess: document.getElementById("reservation-success"),
+    sectionReserve: document.getElementById("section-reservation"),
+    modalReservation: document.getElementById("modal-reservation"),
+    sectionForm: document.getElementById("section-form"),
+    alertForm: document.getElementById("alert-form"),
+    reservationContainer: document.getElementById("reservation-container"),
+    reservationTitle: document.getElementById("reservation-title"),
+    reservationTimer: document.getElementById("reservation-timer"),
+    modalReservation: document.getElementById("modal-reservation"),
+    reservationTime: document.getElementById("reservation-timer"),
+    nomStation: document.getElementById("nom-station"),
+    formFooter: document.getElementById("form-footer"),
+    mapLegend: document.getElementById("map-legend"),
+    canvas: document.getElementById("canvas"),
+    buttonResetMap: document.getElementById("reset-map"),
+    manageForm: function () {
+        this.buttonResetMap.addEventListener("click", function () {
             carte.viewMap.setView([45.764043, 4.835659], 13);
         })
-        buttonResetMap.onclick = function() {
+        this.buttonResetMap.onclick = function () {
             carte.viewMap.closePopup();
         }
+    },
+    showReservation() {
+        this.buttonReserve.addEventListener("click", function () {
+            form.sectionReserve.style.display = "block";
+            form.mapLegend.style.display = "none";
+            form.buttonReserve.style.display = "none";
+            form.buttonErase.style.display = "inline-block";
+        });
+    },
+    reservationSuccess() {
+        this.buttonSucess.addEventListener("click", () => {
+            // Pour recommencer Ã  0 le decompte si l'on reserve un velib sur une autre station
+            clearInterval(form.x);
 
-        var buttonReserve = document.getElementById("button-reservation");
-        var buttonErase = document.getElementById("canvas-delete");
-        var sectionReserve = document.getElementById("section-reservation");
-        var mapLegend = document.getElementById("map-legend");
-        var canvas = document.getElementById("canvas");
+            sessionStorage.setItem("name", this.nomStation.textContent);
+
+            // Gestion des differents elements
+            form.buttonSucess.style.display = "none";
+            form.sectionForm.style.display = "none";
+            form.alertForm.style.display = "block";
+            form.alertForm.innerHTML = `
+                <i class="fas fa-exclamation-triangle"></i>
+                Si vous voulez réserver un autre Vélib à une autre station, <span class="font-weight-bold">veuillez annuler la réservation en cours</span>.
+            `;
+            form.alertForm.classList.add("alert-danger");
+            form.alertForm.classList.remove("alert-warning");
+            form.mapLegend.style.display = "block";
+
+            // On declenche le decompte de la reservation
+            form.reservationTimer();
+        });
+    },
+    reservationTimer(dateEnCours) {
+        // On cree une variable "dateFin" vide
+        let dateFin;
         
-        buttonReserve.addEventListener("click", function() {
-            buttonReserve.style.display = "none";
-            mapLegend.style.display = "none";
-            sectionReserve.style.display = "block";
-            buttonErase.style.display = "inline-block";
-            canvas.style.border="1px solid black";
+        // On utilise la valeur "dateEnCours" dans la variable "dateFin"
+        if(dateEnCours) {
+            dateFin = new Date().getTime() + Number(dateEnCours);
+         // Si dateEnCours est vide on stocke la date actuel + 20 minutes
+        } else { 
+            dateFin = new Date().getTime() + 1200000; // 1 200 000 = 20 minutes en millisecondes
+        }
+
+        // On utilise la methode setInterval pour repeter la fonction
+        this.x = setInterval(() => {
+            const dateActuel = new Date().getTime();
+            const distance = dateFin - dateActuel;
+
+            // On transforme les millisecondes en minutes et secondes
+            let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+            let secondes = Math.floor((distance % (1000 * 60)) / 1000);
             
-            
-        })
+            // On stock la date actuel en milliseconde
+            sessionStorage.setItem("temps", distance);
+
+            // Permet d'afficher la reservation dans le container de la reservation en cours
+            form.reservationSuccessDisplay(minutes, secondes);
+
+           // Si le decompte arrive Ã  0, la reservation expire
+            if (distance < 0) {
+                form.expiredReservation();
+            }
+        });
+    },
+    reservationSuccessDisplay(minutes, secondes) {
+        form.reservationContainer.classList.add("alert-success");
+        form.reservationContainer.classList.remove("alert-secondary");
+        form.reservationContainer.classList.remove("alert-danger");
+        form.reservationTitle.classList.remove("text-center", "mb-0");
+        form.reservationTime.style.display = "block";
+        form.reservationTitle.textContent = " Votre réservation est validée !";
+        form.reservationTime.innerHTML = `Vous avez réservé 1 vélib à la station <span class="font-weight-bold">${sessionStorage.name}</span>. <br> Expire dans : <span class="font-weight-bold">${minutes} minute(s) et ${secondes} seconde(s)</span>.`
+        form.modalReservation.style.display = "inline-block";
+    },
+
+    /**
+     * Permet d'annuler une rÃ©servation
+     */
+    deleteReservation() {
+        this.buttonReservationDelete.addEventListener("click", () => {
+            form.reservationContainer.classList.remove("alert-success");
+            form.reservationContainer.classList.add("alert-danger");
+            form.reservationTitle.textContent = "Votre réservation est annulée !";
+            form.reservationTitle.classList.add("text-center", "mb-0");
+            form.reservationTime.style.display = "none";
+            form.alertForm.innerHTML = `
+                <i class="fas fa-exclamation-triangle"></i>
+                Veuillez sélectionner une station !
+            `;
+            form.alertForm.classList.add("alert-warning");
+            form.alertForm.classList.remove("alert-danger");
+            form.modalReservation.style.display = "none";
+
+            clearInterval(form.x);
+            sessionStorage.clear();
+        });
+    },
+
+    /**
+     * Gestion de l'affichage Ã  l'expiration de la rÃ©servation
+     */
+    expiredReservation() {
+        form.reservationContainer.classList.remove("alert-success");
+        form.reservationContainer.classList.add("alert-danger");
+        form.reservationTitle.textContent = "Votre réservation a expiré !";
+        form.reservationTitle.classList.add("text-center", "mb-0");
+        form.reservationTime.style.display = "none";
+        form.buttonReservationDelete.style.display = "none";
+        form.alertForm.innerHTML = `
+            <i class="fas fa-exclamation-triangle"></i>
+            Veuillez sélectionner une station !
+        `;
+        form.alertForm.classList.add("alert-warning");
+        form.alertForm.classList.remove("alert-danger");
+
+        clearInterval(form.x);
+        sessionStorage.clear();
+    },
+
+    /**
+     * Gestion du webStorage si il y a une rÃ©servation en cours
+     */
+    webStorage() {
+        // On stock la date du session storage dans une variable
+        let dateEnCours = sessionStorage.getItem("temps");
+
+        // Si la valeur de "temps" est supÃ©rieur Ã  0, on reprends la rÃ©servation en cours
+        if (dateEnCours > 0 ) {    
+            // On relance le timer avec la date Ã  laquelle elle s'est arrÃªtÃ©
+            form.reservationTimer(dateEnCours);
+        }
     }
 }
